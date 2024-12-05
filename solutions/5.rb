bcebf5354a1c010c24283b485adf8d603866a7f4
agentshowers <i.psyduck@gmail.com>
2024-12-05 13:57:18 +0000

    faster day 5

--- solutions/5.rb
+++ solutions/5.rb
@@ -6,64 +6,40 @@ class Day5 < Base
   DAY = 5
 
   def initialize(type = "example")
-    lines = Parser.lines(DAY, type)
-    @rules = {}
-    @numbers = []
-    lines.each do |line|
-      if line.include?("|")
-        l, r = line.split("|").map(&:to_i)
-        @rules[r] ||= []
-        @rules[r] << l
-      elsif line != ""
-        @numbers << line.split(",").map(&:to_i)
+    a, b = Parser.read(DAY, type).split("\n\n")
+    predecessors = {}
+    a.split("\n").each do |line|
+      l, r = line.split("|").map(&:to_i)
+      predecessors[r] ||= []
+      predecessors[r] << l
+    end
+    numbers = b.split("\n").map { |n| n.split(",").map(&:to_i) }
+
+    @result = calculate(predecessors, numbers)
+  end
+
+  def calculate(predecessors, numbers)
+    numbers.map do |ns|
+      valid = true
+      middle = nil
+      ns.each_with_index do |n, i|
+        p_count = predecessors[n].intersection(ns).length
+        valid = false if p_count > i
+        middle = n if p_count == ns.length/2
       end
+      [valid, middle]
     end
   end
 
   def one
-    sum = 0
-    @numbers.each do |ns|
-      sum += ns[ns.length / 2] if valid?(ns)
-    end
-    sum
-  end
-
-  def valid?(ns)
-    forbidden = {}
-    ns.each do |n|
-      return false if forbidden[n]
-      (@rules[n] || []).each { |x| forbidden[x] = true }
-    end
-    true
+    @result.map do |valid, middle|
+      valid ? middle : 0
+    end.sum
   end
 
   def two
-    sum = 0
-    @numbers.each do |ns|
-      if !valid?(ns)
-        sort(ns)
-        sum += ns[ns.length / 2]
-      end
-    end
-    sum
-  end
-
-  def sort(ns)
-    (0..ns.length-2).each do |i|
-      changes = false
-      loop do
-        (i+1..ns.length-1).each do |j|
-          if @rules[ns[i]] && @rules[ns[i]].include?(ns[j])
-            a = ns[i]
-            ns[i] = ns[j]
-            ns[j] = a
-            changes = true
-          else
-            changes = false
-          end
-        end
-        break unless changes
-      end
-    end
+    @result.map do |valid, middle|
+      valid ? 0 : middle
+    end.sum
   end
 end
