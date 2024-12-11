a721fb1514db5b646b795ca16f7efe3b752fb4d0
agentshowers <i.psyduck@gmail.com>
2024-12-11 21:57:40 +0000

    remove unused method

--- solutions/11.rb
+++ solutions/11.rb
@@ -48,21 +48,4 @@ class Day11 < Base
     @stones = new_stones
   end
 
-  def expand(stone, missing)
-    return 1 if missing == 0
-    key = "#{stone},#{missing}"
-
-    @memo[key] ||= begin
-      if stone == 0
-        expand(1, missing-1)
-      else
-        base10 = Math.log10(stone).floor + 1
-        if base10.even?
-          expand(stone / 10.pow(base10/2), missing-1) + expand(stone % 10.pow(base10/2), missing-1)
-        else
-          expand(stone*2024, missing-1)
-        end
-      end
-    end
-  end
 end
