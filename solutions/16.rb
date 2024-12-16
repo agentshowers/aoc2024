ec751c9077e6fc5a57c3eda2c7e4e5f18f895496
Joao Amorim <i.psyduck@gmail.com>
2024-12-16 12:12:55 +0000

    revert bad 16

--- solutions/16.rb
+++ solutions/16.rb
@@ -34,18 +34,21 @@ class Day16 < Base
   end
   
   def explore
-    @distances["#{@start[0]},#{@start[1]}"] = 0
-    @parents["#{@start[0]},#{@start[1]}"] = []
+    @distances["#{@start[0]},#{@start[1]},#{RIGHT}"] = 0
+    @parents["#{@start[0]},#{@start[1]},#{RIGHT}"] = []
     queue = Containers::PriorityQueue.new
     queue.push(@start + [RIGHT], 0)
     visited = {}
     while !queue.empty?
       x, y, dir = queue.pop
-      key = "#{x},#{y}"
+      key = "#{x},#{y},#{dir}"
+      next if visited[key]
+      visited[key] = true
       [[dir, false], [turn_left(dir), true], [turn_right(dir), true]].each do |n_dir, turn|
         nx, ny = apply_dir(x, y, n_dir)
         next if @grid[nx][ny] == "#"
-        n_key = "#{nx},#{ny}"
+        n_key = "#{nx},#{ny},#{n_dir}"
+        next if visited[n_key]
         cost = @distances[key] + 1 + (turn ? 1000 : 0)
         if !@distances[n_key] || @distances[n_key] > cost
           @distances[n_key] = cost
