62ba3b263d1f1d89d682f5378f99d4839222241f
Joao Amorim <i.psyduck@gmail.com>
2024-12-17 11:34:05 +0000

    day 17 v1

---
+++ solutions/17.rb
@@ -0,0 +1,86 @@
+require "./lib/parser.rb"
+require "./lib/base.rb"
+require "./lib/grid/matrix.rb"
+
+class Day17 < Base
+  DAY = 17
+
+  def initialize(type = "example")
+    lines = Parser.lines(DAY, type)
+    @original_A = lines[0].scan(/\d+/)[0].to_i
+    @instructions = lines[4].scan(/\d+/).map(&:to_i)
+    @base = Array.new(8)
+  end
+
+  def one
+    simulate(@original_A).join(",")
+  end
+
+  def two
+    i = @instructions.length - 1
+    a = 8.pow(i)
+    while i > 0
+      loop do
+        vs = simulate(a)
+        break if vs[i..] == @instructions[i..]
+        a += 8.pow(i-1)
+      end
+      i -= 1
+    end
+    a
+  end
+
+  def simulate(a_value)
+    @A = a_value
+    @B = 0
+    @C = 0
+    i = 0
+    values = []
+    while i < @instructions.length
+      jump, out = apply(@instructions[i], @instructions[i+1])
+      values << out if out
+      if jump
+        i = jump
+      else
+        i += 2
+      end
+    end
+    values
+  end
+
+  def apply(operation, operand)
+    jump, output = nil
+    case operation
+    when 0
+      @A = @A / 2.pow(combo(operand))
+    when 1
+      @B = @B ^ operand
+    when 2
+      @B = combo(operand) % 8
+    when 3
+      jump = operand if @A != 0
+    when 4
+      @B = @B ^ @C
+    when 5
+      output = combo(operand) % 8
+    when 6
+      @B = @A / 2.pow(combo(operand))
+    when 7
+      @C = @A / 2.pow(combo(operand))
+    end
+    [jump, output]
+  end
+
+  def combo(operand)
+    case operand
+    when 4
+      @A
+    when 5
+      @B
+    when 6
+      @C
+    else
+      operand
+    end
+  end
+end
